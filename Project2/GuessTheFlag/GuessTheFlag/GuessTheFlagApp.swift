2372b01875daeb9b6b4a4495d2efc2535b6288ba
sebastian <sebbec@posteo.de>
2023-12-02 14:29:42 +0100

    Day 21

---
+++ Project2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sebastian on 30.11.23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
